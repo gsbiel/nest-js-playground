a12a699b033e9ab47989591141b87038d9cada06
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-08-29 17:06:07 -0300

    add more  get endpoins in catergory controller supporting querry strings and route params

--- src/controllers/category/category.controller.ts
+++ src/controllers/category/category.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Post, Param, Body, Query } from '@nestjs/common';
+import { Request } from 'express';
 
+// Create an endpoint: /category
 @Controller('category')
 export class CategoryController {
 
+    // StatusCode: 201 by default
+    @Post()
+    createCategory(@Body() createCategoryDto: string):string{
+        return "<h1>A new category has been created</h1>"
+    }
+
+    // Create a GET handler over the "/category" endpoint
+    // @Get("description") would create a GET handler over the "/category/description" endpoint
+    // StatusCode: 200 by default
+    @Get()
+    getAllCategories(@Req() request: Request, @Query('name') name:string):string{
+        if(name){
+            return `<h1>All categories has been returned for querry=${name}.</h1>`
+        }else{
+            return "<h1>All categories has been returned.</h1>"
+        }
+    }
+
+    // http://localhost:3003/category/5/gaspar
+    @Get(':id/:name')
+    getCategoryById(
+        @Param('id') id:string, 
+        @Param('name') name:string):string
+    {
+        return `<h1>Category(id=${id} and name=${name}) has been returned.</h1>`
+    }   
+
+    // @Get('search')
+    // getCategoryByName(@Param('name') name:string):string{
+    //     return `<h1>Category(name= ${name}) has been returned.</h1>`
+    // }
     
 }
